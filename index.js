58eaac9f913577011f11b0d64ff41f60d72c0132
Mohammad <moh.hossain98@gmail.com>
2023-11-14 15:50:26 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+// ! What is git?
+// * Version Control System
+// * Keeps track of changes to our code over time
+
+// ! What is GitHub?
+// * A website to host our git repositories
+// * A social network of programmers
+
+// ? Common vocabularies
+// * Repository - A collection of files and folders that are tracked by git
+// * Add - Adding files and folders to be tracked by git
+// * Commit - A snapshot of our files at a point in time
+// * Branch - A series of commits, typically stemming from a main branch
+// * Merge - Merging combines branches together
+// * Clone - A copy of a repository
+// * Fork - A copy of a repository on GitHub owned by you
+
+// ! Git commands
+// * git init - Initializes a git repository in our current working directory
+// * git status - Shows the current working tree status
+// * git add <file> - Adds a specific file to be tracked
+// * git add . - Adds the current directory to be tracked
+// * git commit -m "message" - Creates a commit with files from the staging area
+// * git log - Shows a history of our commits
+// * git checkout <commit> - Checks out a specific commit
+// * git checkout master - Checks out the master branch
+// * git branch <branch-name> - Creates a new branch
+// * git branch - Shows a list of all branches
+// * git merge <branch-name> - Merges branch into main branch
+// * git clone <url> - Clones a repository from the url
+// * git push origin <branch-name> - Pushes commits to the branch on GitHub
+// * git pull origin <branch-name> - Pulls commits from the branch on GitHub
+
+// ! GitHub commands
+// * git remote add origin <url> - Sets the url of the repo
+// * git remote -v - Shows a list of remotes
